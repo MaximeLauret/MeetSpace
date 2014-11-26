c62e2cc2ed848245619b48c39319f5515cbb6f52
pierrick <veran@intechinfo.fr>
2014-11-26 19:50:29 +0100

    Ajout de fonction dans Master

--- SCRIPT/master.sh
+++ SCRIPT/master.sh
@@ -23,24 +23,59 @@ function add_Mail
 	echo " Function add_Mail"
 }
 
+function add_Unix
+{ 
+	echo " Function add_Unix"
+}
+
+function add_MySecureShell
+{ 
+	echo " Function add_MySecureShell"
+}
+
+function add_Quota
+{ 
+	echo " Function add_Quota"
+}
+
+function add_Sudoers
+{ 
+	echo " Function add_Sudoers"
+}
+
+function add_Web
+{ 
+	echo " Function add_Web"
+}
+
 function add_Share
-{
+{ 
 	echo " Function add_Share"
 }
 
-function add_Vhost
-{
-	echo " Function add_Vhost"
+function add_Pad
+{ 
+	echo " Function add_Pad"
 }
 
-function add_Pad
-{
-	echo " Function add_Pad"
+function add_Mail
+{ 
+	echo " Function add_Mail"
+}
+
+function add_Vhost
+{ 
+	echo " Function add_Vhost"
 }
 
 function add_Dns
 { 
 	echo " Function add_Dns"
+}
+
+function add_PhpMyAdmin
+{ 
+	echo " Function add_PhpMyAdmin"
 }
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -52,19 +87,49 @@ function del_Mail
 	echo " Function del_Mail"
 }
 
+function del_Unix
+{ 
+	echo " Function del_Unix"
+}
+
+function del_MySecureShell
+{ 
+	echo " Function del_MySecureShell"
+}
+
+function del_Quota
+{ 
+	echo " Function del_Quota"
+}
+
+function del_Sudoers
+{ 
+	echo " Function del_Sudoers"
+}
+
+function del_Web
+{ 
+	echo " Function del_Web"
+}
+
 function del_Share
-{
+{ 
 	echo " Function del_Share"
 }
 
-function del_Vhost
-{
-	echo " Function del_Vhost"
+function del_Pad
+{ 
+	echo " Function del_Pad"
 }
 
-function del_Pad
-{
-	echo " Function del_Pad"
+function del_Mail
+{ 
+	echo " Function del_Mail"
+}
+
+function del_Vhost
+{ 
+	echo " Function del_Vhost"
 }
 
 function del_Dns
@@ -72,6 +137,10 @@ function del_Dns
 	echo " Function del_Dns"
 }
 
+function del_PhpMyAdmin
+{ 
+	echo " Function del_PhpMyAdmin"
+}
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -88,20 +157,30 @@ then
 
 	case $1 in
 		add)
-			add_Mail
-			add_Share
-			add_Vhost
-			add_Pad
-			add_Mail
-			add_Dns	
+		add_Unix
+		add_MySecureShell
+		add_Quota
+		add_Sudoers
+		add_Web
+		add_Share
+		add_Pad
+		add_Mail
+		add_Vhost
+		add_Dns
+		add_PhpMyAdmin	
 		;;
 		del) 
-			del_Mail
-			del_Share
-			del_Vhost
-			del_Pad
-			del_Mail
-			del_Dns
+		del_Unix
+		del_MySecureShell
+		del_Quota
+		del_Sudoers
+		del_Web
+		del_Share
+		del_Pad
+		del_Mail
+		del_Vhost
+		del_Dns
+		del_PhpMyAdmin	
 	 	;;
 		*) afficher_Menu
 	esac
