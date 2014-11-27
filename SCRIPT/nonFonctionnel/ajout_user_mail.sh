20493f1acc1a6aeea09b32972c7eefec903c6614
guillaume <guezou@intechinfo.fr>
2014-11-27 10:45:06 +0100

    ajout d'un test d'existence du dossier mail utilisateur

--- SCRIPT/nonFonctionnel/ajout_user_mail.sh
+++ SCRIPT/nonFonctionnel/ajout_user_mail.sh
@@ -8,14 +8,13 @@ password="$1$qpt4xvQ/$8xGJnUaEKpIsEkAWMLoia0"
 	service postfix restart
 	
 #Création Maildir utilisateur
-	mkdir /var/mail/$user
-	chown vmail:vmail /var/mail/$user
-	maildirmake /var/mail/$user/Maildir
-	chown vmail:vmail /var/mail/$user/Maildir
-	
+	if [test -r /var/mail$user]
+		else
+			mkdir /var/mail/$user
+			chown vmail:vmail /var/mail/$user
+			maildirmake /var/mail/$user/Maildir
+			chown vmail:vmail /var/mail/$user/Maildir
+	fi
 #Création authentication IMAP
 	userdb "$user@meetspace.itinet.fr" set imappw=$password home=/var/mail/$user/ mail=/var/mail/$user uid=1006 gid=1006
-#	userdbpw -md5 | userdb "$user@meetspace.itinet.fr" set imappw
-#	echo "$password\n"
-#	echo "$password\n"
 	makeuserdb
