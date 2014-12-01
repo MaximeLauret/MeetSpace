54f0a0be7ff8fa03d14fca474125c0b514fbecf8
guillaume <guezou@intechinfo.fr>
2014-12-01 09:35:56 +0100

    maj pour vérifier le bon fonctionnement des sed

--- SCRIPT/nonFonctionnel/del_user_mail.sh
+++ SCRIPT/nonFonctionnel/del_user_mail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-user=projet3
+user=projet4
 #Suppression utilisateur
 	#Retrait sur postfix
 	sed '/$user/d' /etc/postfix/mailboxmap
@@ -8,7 +8,7 @@ user=projet3
 	service postfix restart
 	
 	#Retrait sur courier-IMAP
-	sed '/$user/d' /etc/courier/userdb
+	sed '/$user@meetspace.itinet.fr/d' /etc/courier/userdb
 	makeuserdb
 
 	#Retrait du dossier mail utilisateur
