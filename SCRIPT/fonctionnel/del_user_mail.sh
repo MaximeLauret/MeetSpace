b3b8476b15969b706d29e914fac6cb8619930fe0
root <root@meetspace.itinet.fr>
2014-12-01 11:27:26 +0100

    ajout des variables en début de fichier pour bon fonctionnement

--- SCRIPT/fonctionnel/del_user_mail.sh
+++ SCRIPT/fonctionnel/del_user_mail.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+user=$1
 #Suppression utilisateur
 	#Retrait sur postfix
 	sed -i "/${user}/d" /etc/postfix/mailboxmap
 	postmap /etc/postfix/mailboxmap
 	service postfix reload
-	
+
 	#Retrait sur courier-IMAP
 	sed -i "/${user}/d" /etc/courier/userdb
 	makeuserdb
