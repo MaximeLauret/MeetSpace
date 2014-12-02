cbfa66651ee23168ced952b844d8a664689a0100
guillaume <guezou@intechinfo.fr>
2014-12-02 13:58:10 +0100

    erreur : remplit bien le fichier avec le mdp demandé mais le prompt est toujours présent et non pris en compte par le fichier.
    test des différentes configurations possibles pour le faire marcher.

--- SCRIPT/fonctionnel/ajout_user_mail.sh
+++ SCRIPT/fonctionnel/ajout_user_mail.sh
@@ -17,7 +17,8 @@ password=$2
 	fi
 #Création authentication IMAP
 	userdb "$user@meetspace.itinet.fr" set home=/var/mail/$user/ mail=/var/mail/$user uid=1006 gid=1006
-	userdbpw -md5 |userdb "$user@meetspace.itinet.fr" set imappw <<-HERE
+	<<-HERE
+	userdbpw -md5 |userdb "$user@meetspace.itinet.fr" set imappw
 	$password
 	$password
 	HERE
