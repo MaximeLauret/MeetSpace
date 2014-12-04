aecc21d88c7565987f61dbf24c1b2c5b1d65d446
thibaultv2 <veran@intechinfo.fr>
2014-12-04 11:34:18 +0100

    Tous les scripts présents dans " fonctionnel " ont étés implémentés dans master. Master n'a pas encore été testé j'attend d'avoir une sauvegarde du serveur avant de le faire. Le script master_test.sh permet de testé complétement le script master
    L'architecture de master a changé pour pouvoir enable et disable un blog

--- SCRIPT/master.sh
+++ SCRIPT/master.sh
@@ -18,8 +18,6 @@ function afficher_Menu
 
 function add_Mail
 { 
-	echo " Function add_Mail"
-
 	#Ajout utilisateur postfix
 	echo "$name@meetspace.itinet.fr $name/" >> /etc/postfix/mailboxmap
 	postmap /etc/postfix/mailboxmap
@@ -29,36 +27,56 @@ function add_Mail
 	if [ ! -r /var/mail/$name ]
 		then
 			mkdir /var/mail/$name
-			chown vmail:vmail /var/mail/$name
 			maildirmake /var/mail/$name/Maildir
-			chown vmail:vmail /var/mail/$name/Maildir
+			chown -R vmail:vmail /var/mail/$name
 	fi
 
 	#Création authentication IMAP
-	userdb "$name@meetspace.itinet.fr" set imappw=$password home=/var/mail/$name/ mail=/var/mail/$name uid=1006 gid=1006
+	md5password=$(userdbpw -md5 <<-EOF
+		$password
+		$password
+	EOF
+	)
+	userdb "$name@meetspace.itinet.fr" set imappw=$md5password home=/var/mail/$name/ mail=/var/mail/$name uid=1006 gid=1006 
 	makeuserdb
-
+	
+	#Initialisation du dossier principal de la boite mail
+	telnet mail.meetspace.itinet.fr 25 <<-AUTO
+	mail from:<contact@meetspace.itinet.fr>
+	rcpt to:<$name@meetspace.itinet.fr>
+	data
+	Subject: Bienvenue sur Meetspace
+	L'équipe de Meetspace vous souhaite la bienvenue sur son site.
+	.
+	quit
+	AUTO
 }
 
 function add_Unix
 { 
 	echo " Function add_Unix"
+	#Script d'ajout d'un utilisateur Unix sur le serveur Meetspace
+
+	#Ajout de l'utilisateur Unix:
+
+	cd /var/sftp/home/
+	#Chiffrage du mdp
+	mkpassword=`mkpasswd $password`
+
+	#Ajout de l'utilisateur
+	useradd --home /var/sftp/home/$name --gid meetspace_user --password $mkpassword $name --shell "/bin/MySecureShell"
+
+	#Création du répertoire personnel de l'utilisateur
+	mkdir $name
+	chown -R $name $name
+	chgrp -R meetspace_user $name
+
+	#Mise en place des quotas:
+	setquota -u toto 300 300 0 0 -a /var/sftp/home/$name
+	setquota -u toto 200 200 0 0 -a /var/mail/$name
+
 }
 
-function add_MySecureShell
-{ 
-	echo " Function add_MySecureShell"
-}
-
-function add_Quota
-{ 
-	echo " Function add_Quota"
-}
-
-function add_Sudoers
-{ 
-	echo " Function add_Sudoers"
-}
 
 function add_Web
 { 
@@ -83,11 +101,26 @@ function add_Mail
 function add_Vhost
 { 
 	echo " Function add_Vhost"
+	# add_vhost.sh
+	# Create a new VHost
+	# Created by Maxime LAURET (2014-11-26)
+
+	# Creating the directory host
+	mkdir -p /var/sftp/$name/www
+	# According writes to the user interface
+	chown -R www-data:www-data /var/sftp/$project_name/www
+	# Editing the configuration file
+	echo "
+	<VirtualHost *:80>
+	ServerName $project_name.meetspace.itinet.fr
+	DocumentRoot /var/sftp/$project_name
+	</VirtualHost>
+	" > /etc/apache2/sites-available/$name.conf
+
 }
 
 function add_Dns
 { 
-	echo " Function add_Dns"
 	cd /etc/tinydns/root/
 	echo " Ajout de la ligne:"
 	echo "+$NAME.meetspace.itinet.fr:88.177.168.133:86400"
@@ -112,21 +145,14 @@ function del_Mail
 function del_Unix
 { 
 	echo " Function del_Unix"
-}
+	cd /var/sftp/home
 
-function del_MySecureShell
-{ 
-	echo " Function del_MySecureShell"
-}
+	#Suppression de l'utilisateur
+	userdel --force $name 
 
-function del_Quota
-{ 
-	echo " Function del_Quota"
-}
+	#Suppression de son répertoire
+	rm -rf $name
 
-function del_Sudoers
-{ 
-	echo " Function del_Sudoers"
 }
 
 function del_Web
@@ -147,11 +173,32 @@ function del_Pad
 function del_Mail
 { 
 	echo " Function del_Mail"
+	#Suppression utilisateur
+	#Retrait sur postfix
+	sed -i "/${name}/d" /etc/postfix/mailboxmap
+	postmap /etc/postfix/mailboxmap
+	service postfix reload
+
+	#Retrait sur courier-IMAP
+	sed -i "/${name}/d" /etc/courier/userdb
+	makeuserdb
+
+	#Retrait du dossier mail utilisateur
+	rm -r /var/mail/$name
 }
 
 function del_Vhost
 { 
 	echo " Function del_Vhost"
+	# del_vhost.sh
+	# Delete a VHost
+	# Created by Maxime LAURET (2014-12-01)
+
+
+	rm /etc/apache2/sites-available/$name.conf		# Deleting the .conf file
+	rm -R /var/sftp/$name;							# Deleting the directory
+
+	service apache2 reload							# Reloading the Apache service
 }
 
 function del_Dns
@@ -182,7 +229,29 @@ requete=$1
 
 #projectORuser = project ou user 
 projectORuser=$2
+
+#name= Nom d'utilisateur ou de projet
 name=$3
+
+#password= mot de passe utilisateur
+password=$4
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Fonction main 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Déclaration des variables:
+#Requette= " add " ou " del "
+requete=$1
+
+#projectORuser = project ou user 
+projectORuser=$2
+
+#name= Nom d'utilisateur ou de projet
+name=$3
+
+#password= mot de passe utilisateur
 password=$4
 
 #Si il y a bien 4 paramètre en entré
@@ -194,22 +263,19 @@ then
 		case $projectORuser in
 
 			user) 
-				add_Unix
-				add_MySecureShell
-				add_Quota
-				add_Sudoers
-				add_Share
-				#add_Mail
+				#add_Unix
+				#add_Share
+				#add_UserMail
 			;;
 
 			project) 
-				add_Web
-				add_Share #Cette fonction doit créé un rep partager entre tous les membres du projet
-				add_Pad
-				add_Mail #Cette fonction devra créé un mail avec un alias à tous les membres du projet
-				add_Vhost #La fonction Vhost devra pouvoir faire apparaitre ou disparaitre le blog en fonction du choix du chef de projet
-				add_Dns
-				add_PhpMyAdmin
+				#add_Web
+				#add_ShareBox #Cette fonction doit créé un rep partager entre tous les membres du projet
+				#add_Pad
+				#add_Mail #Cette fonction devra créé un mail avec un alias à tous les membres du projet
+				#add_Vhost #La fonction Vhost devra pouvoir faire apparaitre ou disparaitre le blog en fonction du choix du chef de projet
+				#add_Dns
+				#add_PhpMyAdmin
 
 			;;
 
@@ -220,31 +286,49 @@ then
 		del)
 		case $projectORuser in
 			user)
-				add_Unix
-				add_MySecureShell
-				add_Quota
-				add_Sudoers
-				add_Share
-				add_Mail
+				#add_Unix
+				#add_Share
+				#add_Mail
 			;;
 
 			project)
-				add_Web
-				add_Share
-				add_Pad
-				add_Mail
-				add_Vhost
-				add_Dns
-				add_PhpMyAdmin
+				#add_Web
+				#add_Share
+				#add_Pad
+				#add_Mail
+				#add_Vhost
+				#add_Dns
+				#add_PhpMyAdmin
 			;;
 
 			*) afficher_Menu
 		esac
-	 	;;
+		;;
+
+		enable)
+		case $projectORuser in
+			user) echo "Impossible de désactiver/activer un utilisateur pour l'instant" ;;
+
+			project) ;;
+
+
+			*) afficher_Menu
+		esac
+		;;
+
+		disable)
+		case $projectORuser in
+			user) echo "Impossible de d'activer/désactiver un utilisateur pour l'instant";;
+
+			project)  ;;
+
+			*) afficher_Menu
+		esac
+		;;
 
 		*) afficher_Menu
 	esac
 
 else
 	afficher_Menu
-fi 
+fi  
---
+++ SCRIPT/master_test.sh
@@ -0,0 +1,8 @@
+./master.sh add project toto toto
+./master.sh add user toto toto
+./master.sh del project toto toto
+./master.sh del user toto toto
+./master.sh enable user toto toto
+./master.sh disable user toto toto
+./master.sh enable project toto toto
+./master.sh disable project toto toto
