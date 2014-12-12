11c09efe9c3b6e3e027a8856aa505b49e2e9db2b
pierrick <veran@intechinfo.fr>
2014-12-12 10:52:12 +0100

    Ajout du répertoire à sauvegarder : /etc/postfix/

--- SCRIPT/SOD/sav_tar.sh
+++ SCRIPT/SOD/sav_tar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-source sav_config.sh
-source sav_ssh.sh
+#source sav_config.sh
+#source sav_ssh.sh
 
 echo "------------------------------------------------------";
 echo "- Sauvegarde du serveur Meetspace";
@@ -12,7 +12,7 @@ dpkg --get-selections > /home/pierrick/liste-des-paquets
 echo "Création de l'archive";
 
 # Précisez vos répertoire à sauvegarder entre guillemets:
-tar -cvzf /home/pierrick/SOD/date-`date +%Y-%m-%d-%H-%M`.tar.gz "/home/" "/var/sftp/" "/var/mail" "/var/www" "/etc/apache2" "/etc/prosody" "/etc/tinydns/root" "/etc/courier" "/etc/mysql" "/etc/phpmyadmin" " /etc/ssh/sftp_config"
+tar -cvzf /home/pierrick/SOD/date-`date +%Y-%m-%d-%H-%M`.tar.gz "/home/" "/var/sftp/" "/var/mail" "/var/www" "/etc/apache2" "/etc/prosody" "/etc/tinydns/root" "/etc/courier" "/etc/mysql" "/etc/phpmyadmin" " /etc/ssh/sftp_config" "/etc/postfix/"
 
 
 echo "------------------------------------------------------";
@@ -23,8 +23,8 @@ echo "Vérification de l'existence de l'archive";
 if [ -e /votre_support/backup.tar.gz ]
 then
 	echo "Votre archive a bien été créée.";
-	echo " Envoie au serveur "
-	connection_ssh
+	#echo " Envoie au serveur "
+	#connection_ssh
 else
 	echo "Il y a eu un problème lors de la création de l'archive.";
 fi
