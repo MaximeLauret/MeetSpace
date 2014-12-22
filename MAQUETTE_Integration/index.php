4db689d7fbe0ad583b9b3b149b3acb49c4a14ff3
Maxime Lauret <mlauret@intechinfo.fr>
2014-12-22 23:24:42 +0100

    Ajout de la fonctionnalité index.php.  Signin fonctionne nickel.

--- MAQUETTE_Integration/index.php
+++ MAQUETTE_Integration/index.php
@@ -1,66 +1,30 @@
-<!-- Première page sur la quel on arrive
-	Formulaire d'inscription et de connection
+<!--
+index.php
+Controller for the index
+Created by Max (2014-12-22)
 -->
 
-	<?php
-		include("./INCLUDE/header_public.php");
-	?>
+<?php
+	session_start();
+?>
 	
-<!-- Colone de gauche - Description -->
+<?php
+	include ("M/M_index.php");
+?>
+	
+<?php
 
-      
-                <div class="col-xs-12 col-sm-5 col-md-4 col-md-offset-2" >
-                
-                <div class="inner cover">
-                 <h1 class="cover-heading">Bienvenue sur MeetSpace</h1>            
-                </div>
+	$database = log_database();		// Connexion à la base de données
+	
+	if(isset($_POST['signin']) AND isset($_POST['nickname_signin_input']) AND isset($_POST['password_signin_input'])) {		// Sign in
+		$message = register_user($database, $_POST['nickname_signin_input'], $_POST['mail_input'], $_POST['password_signin_input'], $_POST['password_confirmation_input']);
+		echo ("Votre compte a bien été créé.");
+	} else {
+		// Nothing
+	}
 
-                  <p class="lead">
-                    Réunissez-vous où que vous soyez et à tout moment avec MeetSpace, <br> l'espace de réunion en ligne. <br>Créez votre réseau et profitez des services offerts par MeetSpace pour gérer votre projet.</p>
-                  <p class="lead">
-                    <a href="./about.php" class="btn btn-lg btn-default">En savoir plus</a>
-                  </p>
-
-                </div><!-- / Colone de gauche - Description -->
-
-<!--  Colone de Droite - Connexion et Inscription -->
-<div class="col-xs-12 col-sm-5 col-md-2 col-md-offset-2 ">
-
-<!-- Formulaire de connexion-->
-<h3>Connexion</h3>
-<form class="form-horizontal">
-	<!-- Pseudo input-->
-	<input id="pseudoinput" name="pseudoinput" type="text" placeholder="pseudo" class="form-control input-md" required="">
-	<br/>
-	<!-- Password input-->
-	<input id="passwordinput" name="passwordinput" type="password" placeholder="password" class="form-control input-md" required="">
-	<!-- Button connexion -->
-	<button id="subbmit" name="subbmit" class="btn btn-primary btn-lg ">Connexion</button>
-</form>
-
-<!-- Formulaire d'inscription -->
-<h3>Inscription</h3>
-	<form class="form-horizontal" action="#" method="POST">
-                                        
-                    <!-- Pseudo -->
-                    <input id="pseudoinput" name="nickname_r" type="text" placeholder="pseudo" class="form-control input-md" required="">
-                      
-                    <!-- Mail -->
-                    <input id="pseudoinput" name="mail_r" type="text" placeholder="mail" class="form-control input-md" required="">
-
-                    <!-- Premier mot de passe -->
-                    <input id="passwordinput" name="pwd_r1" type="password" placeholder="Mot de passe" class="form-control input-md" required="">
-                      
-                    <!-- Validation du mot de passe -->
-                    <input id="passwordinput" name="pwd_r2" type="password" placeholder="Comfirmation du mot de passe" class="form-control input-md" required="">
-
-                  <!-- Validation -->
-                    <button id="subbmit" name="signin" class="btn btn-success btn-lg" value="Log in">Inscription</button>
-                  </form>
-  <!-- Fin formulaire d'inscription -->
-
-                <!-- / Colone de Droite - Connection et Inscription -->
-
-              </section>
-
-<?php include("./INCLUDE/footer_public.php"); ?>
+?>
+	
+<?php
+	include ("V/V_index.php");
+?>
