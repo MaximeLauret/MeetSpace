a867ef4dd4edfedbe0d49f06f53177549bb18415
Guillaume <guezou@intechinfo.fr>
2015-01-05 10:55:18 +0100

    Ajout script de modification de password pour le compte Chat

---
+++ SCRIPT/fonctionnel/modif_passwd_userChat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Variables : Nom et Password utilisateur
+
+if [ ! -e /var/lib/prosody/meetspace%2eitinet%2efr/accounts/$name ]
+then
+	/usr/bin/prosodyctl passwd $name@meetspace.itinet.fr <<-EOF
+		$password
+		$password
+		EOF
+	/usr/bin/prosodyctl restart
+fi
