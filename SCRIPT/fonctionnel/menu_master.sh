f47046f2a63a6e13e22cf6c06147e90e3a8ab8db
Pierrick VERAN <veran@intechinfo.fr>
2015-01-06 14:15:56 +0100

    Master: Préparation du script de test et modification du menu

--- SCRIPT/fonctionnel/menu_master.sh
+++ SCRIPT/fonctionnel/menu_master.sh
@@ -7,21 +7,23 @@ echo "Outils d'administration du serveur meetspace
 Utilisation: 
 
 Gestion des utilisateurs:
-master user add [name] [password]
-master user del [name]
+	master user add [name] [password]
+	master user del [name]
 
 Gestion des projets:
-master project add [name]
-master project del [name]
+	master project add [name]
+	master project del [name]
 
 Gestion des virtualhost du site de projet:
-master project vhost enable [name]
-master project vhost disable [name]
+	master project vhost enable [name]
+	master project vhost disable [name]
 
 Gestion des alias de la boite contact de projet:
-master project alias add [name]
-master project alias del [name]
+	Ajouter l'alias d'un projet:
+	master project alias add [name]
+	master project alias del [name]
 
-master project alias add [projectName] [userName]
-master project alias del [projectName] [userName]
+	Ajouter un utilisateur à l'alias d'un projet:
+	master project alias add [projectName] [userName]
+	master project alias del [projectName] [userName]
 "
