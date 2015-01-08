301694c1daecb8639ad1688984aa7a7324446875
Pierrick VERAN <veran@intechinfo.fr>
2015-01-08 22:20:57 +0100

    Php index: Test du session ID

--- MAQUETTE_Integration/index.php
+++ MAQUETTE_Integration/index.php
@@ -6,8 +6,28 @@ Created by Max (2014-12-22)
 
 <?php
 	session_start();
-?>
 	
+?>
+	<?php
+/**
+* @return bool
+*/
+function is_session_started() //Source: http://php.net/manual/fr/function.session-status.php
+{
+		if ( php_sapi_name() !== 'cli' ) {
+			if ( version_compare(phpversion(), '5.4.0', '>=') ) {
+				return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+			} else {
+				return session_id() === '' ? FALSE : TRUE;
+			}
+		}
+		return FALSE;
+	}
+	// Example
+	if ( is_session_started() === FALSE ) session_start();
+	echo session_id();
+?>
+
 <?php
 	include ("./M/M_index.php");
 ?>
