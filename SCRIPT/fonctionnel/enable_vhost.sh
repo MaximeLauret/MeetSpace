e7f0bfb753ec2f4c1d7a09dc163cee9d972f9134
Guillaume <guezou@intechinfo.fr>
2015-01-20 14:11:03 +0100

    retrait de la sécurité pour des raisons de test

--- SCRIPT/fonctionnel/enable_vhost.sh
+++ SCRIPT/fonctionnel/enable_vhost.sh
@@ -11,9 +11,9 @@ name=$1
 
 #Script
 
-if (($#=="1"));then
+#if (($#=="1"));then
 	/bin/ln -s /etc/apache2/sites-available/$name.conf /etc/apache2/sites-enabled/$name.conf
 	sudo /usr/sbin/service apache2 reload							# Reloading the Apache service
-else
-	echo " enable_vhost: Nombre de paramètres invalide "
-fi
+#else
+#	echo " enable_vhost: Nombre de paramètres invalide "
+#fi
