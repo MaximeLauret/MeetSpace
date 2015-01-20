96d2e947450ac20b4a4f35e3c92c4c767ece26ab
Pierrick VERAN <veran@intechinfo.fr>
2015-01-20 20:50:09 +0100

    Nous avons maintenant une favicon. Reste à la mettre en 16x16

--- MAQUETTE_Integration/V/INCLUDE/header.php
+++ MAQUETTE_Integration/V/INCLUDE/header.php
@@ -5,6 +5,7 @@ header.php
 <head>
 
 	<title>MeetSpace</title>
+	<link rel="icon" href="./IMG/favicon.ico" type="image/x-icon"/>
 	<meta http-equiv = "Content-Type" content = "text/html" charset = "UTF-8";>
 	<link href = "http://getbootstrap.com/dist/css/bootstrap.min.css" rel = "stylesheet">		<!-- CSS BootStrap -->
 	<link href = "http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel = "stylesheet">		<!-- Inclusion des icones -->
