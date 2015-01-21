b50b82480e6211a91f78912d8d2ec7a4a2064b6d
Maxime Lauret <mlauret@intechinfo.fr>
2015-01-21 10:40:13 +0100

    Mise à jour et correction

--- MAQUETTE_Integration/C_myprojects.php
+++ MAQUETTE_Integration/C_myprojects.php
@@ -29,15 +29,26 @@ if (!isset($_SESSION)) { session_start(); }
 				$project_id_exe = get_project_id ($database, $_POST["project_name_input"]);		// On récupère l'ID du projet.
 				add_author ($database, $project_id_exe);		// On ajoute l'auteur.
 				echo ("Le projet a bien été créé<br/>");
+				echo("<meta http-equiv='Refresh' content='0; url=C_myprojects.php'/>");
 			} else {
 				echo ("Erreur : le projet n'a pas pu être créé");
+				echo("<meta http-equiv='Refresh' content='0; url=C_project.php?".$element['NAME']."'/>");
 			}
+		}
+
+	// AFFICHER PAGE PROJET
+
+		if (isset ($_GET["project_query"])) {
+			echo("<meta http-equiv='Refresh' content='0; url=C_myprojects.php'/>");
 		}
 
 	// QUITTER UN PROJET
 		if (isset ($_POST["leave_project"]) AND isset ($_POST["project_selection"])) {
 			$project_id_to_leave_exe = get_project_id_to_leave ($database, $_POST["project_selection"]);		// On récupère l'id du projet.
 			leave_project ($database, $project_id_to_leave_exe);		// On quitte le projet.
+			echo("<meta http-equiv='Refresh' content='0; url=C_myprojects.php'/>");
+		} else {
+			// NOthing
 		}
 
 ?>
--- MAQUETTE_Integration/V/V_myprojects.php
+++ MAQUETTE_Integration/V/V_myprojects.php
@@ -15,25 +15,39 @@ Created by Max (2014-12-23)
 	<body>
 		
 		<?php include ("./V/INCLUDE/topbar.php"); ?>
-<<<<<<< HEAD
-	
-		
+
 		<?php
-		
-			echo "<h3>Bonjour ".$_SESSION['USER']."</h3>";
-			echo "<h4>Voici vos projets : </h4>";
-			foreach ($projects_list as $element) {
-				echo ("<a href = './../C_project.php?".$element['NAME']."'>".$element['NAME']."</a>");		// Affiche le nom du projet et fait le lien vers la page de celui-ci.
-				echo ("<br/>");
-				echo $element["PROJECT_DESCRIPTION"];		// Affiche la description s'il y a.
-				echo ("<form action = '#' method = 'POST'>".		// Formulaire pour récupérer le nom du projet.		À PLANQUER
-						"<input id = 'text' name = 'project_selection' value = ".$element['NAME'].">
-						<button id = 'submit' name = 'leave_project' value = 'leave_project' > Quitter le projet </button>
+			
+				echo "<h3>Bonjour ".$_SESSION['USER']."</h3>";
+				
+				echo "<h4>Voici vos projets : </h4>";
+				
+				foreach ($projects_list as $element) {
+					
+					echo ("<h4>
+								<a href = './../C_project.php?".$element['NAME']."'>".$element['NAME']."</a>
+							</h4>");		// Affiche le nom du projet et fait le lien vers la page de celui-ci.
+					echo ("<br/>");
+					echo $element["PROJECT_DESCRIPTION"];		// Affiche la description s'il y a.
+					echo ("<form action = './../C_project.php' metod = 'GET'>
+							<button id = 'submit' name = '".$element['NAME']."'> Aller à la page du projet </button>	
 						</form>");
-				echo "<br/>";
-			}
-		
-		?>
+					echo ("<form action = '#' method = 'POST'>".		// Formulaire pour récupérer le nom du projet.		À PLANQUER
+							"<button id = 'submit' name = 'leave_project' value = 'leave_project' > Quitter le projet </button>
+							</form>");
+					echo "<br/>";
+
+				}
+
+				/*if ($i == 0) {
+					echo ("Vous ne collaborez à aucun projet.
+					<br/>
+					Rejoignez-en un ou créez le votre dès maintenant !");
+				} else {
+					// Nothing
+				}*/
+			
+			?>
 		
 		<div class="col-xs-12 col-sm-9 col-md-9 col-lg-3 col-lg-offset">
 			<div  class="project" >
@@ -47,67 +61,8 @@ Created by Max (2014-12-23)
 						<button id = "submit" name = "create_project" class = "btn btn-success btn-lg" value = "create_project"> Créer
 						</button>
 				</form>
-=======
-		<section>
-			<?php
-			
-				echo "<h3>Bonjour ".$_SESSION['USER']."</h3>";
-				echo "<h4>Voici vos projets : </h4>";
-				foreach ($projects_list as $element) {
-					echo ("<a href = './../C_project.php?".$element['NAME']."'>".$element['NAME']."</a>");		// Affiche le nom du projet et fait le lien vers la page de celui-ci.
-					echo ("<br/>");
-					echo $element["PROJECT_DESCRIPTION"];		// Affiche la description s'il y a.
-					echo ("<form action = '#' method = 'POST'>".		// Formulaire pour récupérer le nom du projet.		À PLANQUER
-							"<input id = 'text' name = 'project_selection' value = ".$element['NAME'].">
-							<button id = 'submit' name = 'leave_project' value = 'leave_project' > Quitter le projet </button>
-							</form>");
-					echo "<br/>";
-				echo "Bonjour ".$_SESSION['USER'];
-				echo "<br/> <br/>";
-				echo "Vos projets : ";
-				echo "<br/>";
-				}
-
-				$i = 0;
-				while ($i < 4) {		// On affiche 5 projets.
-					foreach ($projects_list as $element) {
-						echo ("<a href = './../C_project.php?".$element['NAME']."'>".$element['NAME']."</a>");		// Affiche le nom du projet et fait le lien vers la page de celui-ci.
-						echo ("<br/>");
-						echo $element["PROJECT_DESCRIPTION"];		// Affiche la description s'il y a.
-						echo ("<form action = '#' method = 'POST'>".		// Formulaire pour récupérer le nom du projet.		À PLANQUER
-								"<input id = 'text' name = 'project_selection' value = ".$element['NAME'].">
-								<button id = 'submit' name = 'leave_project' value = 'leave_project' > Quitter le projet </button>
-								</form>");
-						echo "<br/>";
-					}
-				$i++;
-				}
-
-				if ($i == 0) {
-					echo ("Vous ne collaborez à aucun projet.
-					<br/>
-					Rejoignez-en un ou créez le votre dès maintenant !");
-				} else {
-					// Nothing
-				}
-			
-			?>
-			<div class="col-xs-12 col-sm-9 col-md-9 col-lg-3 col-lg-offset">
-				<div  class="project" >
-					<form class = "form-horizontal" action = "#" method = "POST">
-					<legend>Lancez votre propre projet</legend>
-						<!-- PROJECT NAME INPUT -->
-							<input id = "pseudoinput" name = "project_name_input" type = "text" placeholder = "Nom du projet" class = "form-control input-md" required = "">
-						<!-- PROJECT DESCRIPTION INPUT -->
-							<input id = "text" name = "project_description_input" type = "text" placeholder = "Description" class = "form-control input-md">
-						<!-- SUBMIT BUTTON -->
-							<button id = "submit" name = "create_project" class = "btn btn-success btn-lg" value = "create_project"> Créer
-							</button>
-					</form>
-				</div>
->>>>>>> 91301f1ad9a77f026e8b29cd0e1c76a1f40b9a92
 			</div>
-		</section>
+		</div>
 
 		<?php
 			include("INCLUDE/footer.php");
