bd85cfe4290247a300837ae353e76439c2368fd1
Guillaume <guezou@intechinfo.fr>
2015-01-28 13:08:56 +0100

    Correction du problème dans del_dns

--- SCRIPT/fonctionnel/del_dns.sh
+++ SCRIPT/fonctionnel/del_dns.sh
@@ -9,10 +9,10 @@
 name=$1
 
 
-#Fonctionnel. Le seul problème c'est que si j'add projet1 et projet10 et que je décide de del projet1. Projet 10 disparait aussi..
+
 /bin/echo " Suppression de la ligne: $name"
 /bin/echo "+"$name".meetspace.itinet.fr:88.177.168.133:86400"
-/bin/sed -i "/${name}/d" /etc/tinydns/root/data
+/bin/sed -i "/${name}\./d" /etc/tinydns/root/data
 /usr/bin/make -C /etc/tinydns/root/
 
 
